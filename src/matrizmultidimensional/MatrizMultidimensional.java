55f6f195fa418913558e1a94c38f5249f772bb8f
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-13 10:44:12 -0600

    Initial commit

---
+++ src/matrizmultidimensional/MatrizMultidimensional.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrizmultidimensional;
+
+/**
+ *
+ * @author Yova
+ */
+public class MatrizMultidimensional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String valores[][] = {{"valor1", "valor2"},{"valor3", "valor4"}};
+        
+        System.out.println(valores[1][0]);
+        
+    }
+    
+}
